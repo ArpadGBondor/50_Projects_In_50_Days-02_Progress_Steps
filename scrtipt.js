8afa20c712214b27df1cce12f66d36c42eed6bb2
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-01-12 23:26:54 +0000

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,44 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+update();
+
+next.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.lenth) {
+    currentActive = circles.lenth;
+  }
+
+  update();
+});
+
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+
+  progress.style.width = ((currentActive - 1) / (circles.length - 1)) * 100 + '%';
+
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
+}
